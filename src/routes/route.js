dc3f57d484fbf8d9b90c0024b9fd06afed6e9bdf
shubhangipendam <shubhangipendam18@gmail.com>
2022-10-13 19:25:04 +0530

    modify index and rutes

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,5 +4,8 @@ const router = express.Router();
 router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
+router.get('/test-you', function (req, res) {
+    res.send('My second ever api!')
+});
 
 module.exports = router;
